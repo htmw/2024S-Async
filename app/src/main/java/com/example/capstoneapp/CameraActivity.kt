d8d143b2c4191110353b0847d07e09d756c5746e
ReneRisa <risael31@gmail.com>
2024-04-20 20:53:32 -0400

    camera activity

--- app/src/main/java/com/example/capstoneapp/CameraActivity.kt
+++ app/src/main/java/com/example/capstoneapp/CameraActivity.kt
@@ -3,6 +3,11 @@ package com.example.capstoneapp
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
@@ -13,7 +18,15 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import com.example.capstoneapp.ml.BestFp16
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
 
 class CameraActivity : AppCompatActivity() {
 
@@ -21,17 +34,28 @@ class CameraActivity : AppCompatActivity() {
     lateinit var textureView: TextureView
     lateinit var handler: Handler
     lateinit var cameraDevice: CameraDevice
+    lateinit var imageView: ImageView
+    lateinit var bitmap: Bitmap
+    lateinit var model: BestFp16
+    val imageProcessor = ImageProcessor.Builder().add(ResizeOp(640,640,ResizeOp.ResizeMethod.BILINEAR)).build()
+    val paint = Paint()
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN,Color.RED, Color.CYAN, Color.BLACK
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
         get_permission()
 
+
+        model = BestFp16.newInstance(this)
         val handlerThread = HandlerThread("videoThread")
         handlerThread.start()
         handler = Handler(handlerThread.looper)
 
         textureView = findViewById(R.id.textureView)
+        imageView = findViewById(R.id.imageView)
         textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
             override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
                 open_camera()
@@ -46,6 +70,45 @@ class CameraActivity : AppCompatActivity() {
             }
 
             override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+
+
+// Creates inputs for reference.
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+
+
+                val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 640, 640, 3), DataType.FLOAT32)
+                inputFeature0.loadBuffer(image.buffer)
+
+// Runs model inference and gets result.
+                val outputs = model.process(inputFeature0)
+                //val outputs = model.process(image)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+                val locations = outputs.locationsAsTensorBuffer.floatArray
+
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+// Releases model resources if no longer used.
+
+                val h = mutable.height
+                val w = mutable.width
+
+                paint.textSize = h/15f
+                paint.strokeWidth = h/85f
+                var x =0
+                scores.forEachIndexed{ index, fl ->
+                    x = index
+                    x *= 4
+                    if(fl > 0.5){
+                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(RectF(locations.get(x+1)*w, locations.get(x)*h, locations.get(x+3)*w, locations.get(x+2)*h), paint)
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(customclasses.get(classes.get(index).toInt())+ " " + fl.toString(), locations.get(x+1)*w, locations.get(x)*h,paint)
+
+                    }
+                }
 
             }
 
@@ -54,6 +117,10 @@ class CameraActivity : AppCompatActivity() {
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
 
     @SuppressLint("MissingPermission")
     fun open_camera(){
